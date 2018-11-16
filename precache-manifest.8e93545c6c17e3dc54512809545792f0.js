3af19778095249714574b820fd7c7d7a77697e27
iburgos72 <ivan.pburgos@gmail.com>
2018-11-15 23:41:06 -0600

    Updates

---
+++ precache-manifest.8e93545c6c17e3dc54512809545792f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13221802a9edae69d6b6",
+    "url": "/avatar-svg/static/js/runtime~main.13221802.js"
+  },
+  {
+    "revision": "481100b03998a6e422e4",
+    "url": "/avatar-svg/static/js/main.481100b0.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/avatar-svg/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "481100b03998a6e422e4",
+    "url": "/avatar-svg/static/css/main.8ea77d17.chunk.css"
+  },
+  {
+    "revision": "74a1291f14e99e4e700f234676300d80",
+    "url": "/avatar-svg/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0c124cde6fedf6506e585c146c63152.js"
+  "/avatar-svg/precache-manifest.8e93545c6c17e3dc54512809545792f0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/avatar-svg/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
