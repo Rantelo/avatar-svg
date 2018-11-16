7002366debed4f43fd5968915c307a8c774a2bad
iburgos72 <ivan.pburgos@gmail.com>
2018-11-15 23:29:19 -0600

    Updates

---
+++ precache-manifest.e0c124cde6fedf6506e585c146c63152.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c34b609a68c309dcde04",
+    "url": "/static/js/main.c34b609a.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "c34b609a68c309dcde04",
+    "url": "/static/css/main.8ea77d17.chunk.css"
+  },
+  {
+    "revision": "e675cada2df633cd9b5c2aa69482c50b",
+    "url": "/index.html"
+  }
+];
